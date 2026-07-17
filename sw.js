5e35d2b66dcd3f91c8431504b3066937c251e486
itscassipy <cassidyseo@gmail.com>
2026-07-16 23:08:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,67 @@
-const CACHE_NAME = 'budget-v1';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'cassidy-budget-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './image_8.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+// Install Event: Cache essential files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+// Fetch Event: Serve from cache if available (Offline-First strategy)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version if found
+                if (response) {
+                    return response;
+                }
+                
+                // Otherwise fetch from network
+                return fetch(event.request).then(
+                    function(networkResponse) {
+                        // Check if we received a valid response
+                        if(!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                            return networkResponse;
+                        }
+
+                        // Clone the response because it's a stream and can only be consumed once
+                        var responseToCache = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return networkResponse;
+                    }
+                );
+            })
+    );
+});
+
+// Activate Event: Clean up old caches if version changes
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
